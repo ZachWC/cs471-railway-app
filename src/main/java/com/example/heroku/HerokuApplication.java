023edbe3ce649209acdece94900168aceaf3f298
Zach Christensen <zachchristensen330@u.boisestate.edu>
2025-12-05 09:46:51 -0700

    fixed circular dependecy

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -16,20 +16,15 @@
 
 package com.example.heroku;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
@@ -37,9 +32,6 @@ import java.util.Map;
 @Controller
 @SpringBootApplication
 public class HerokuApplication {
-
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
 
   @Autowired
   private DataSource dataSource;
@@ -71,17 +63,6 @@ public class HerokuApplication {
     } catch (Exception e) {
       model.put("message", e.getMessage());
       return "error";
-    }
-  }
-
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
     }
   }
 
